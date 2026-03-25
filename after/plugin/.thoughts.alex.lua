c8b4a77e70f88729e28679d337bf8f14018df66d
alexanderinnerbichler <alexander.innerbichler@bernard-gruppe.com>
2026-03-25 13:22:53 +0100

    add .thoughts.alex.lua files

---
+++ after/plugin/.thoughts.alex.lua
@@ -0,0 +1,7 @@
+return {
+  ["harpoon.lua"] = { {
+      linenumber = 26,
+      text = "have not used this in sooo long",
+      timestamp = "12:37:04 16-01-2026 "
+    } }
+}
---
+++ lua/alex/.thoughts.alex.lua
@@ -0,0 +1,15 @@
+return {
+  ["quicker.lua"] = { {
+      linenumber = 6,
+      text = "is this the future?",
+      timestamp = "12:36:41 16-01-2026 "
+    }, {
+      linenumber = 117,
+      text = "why dont you work?",
+      timestamp = "12:50:33 16-01-2026 "
+    }, {
+      linenumber = 128,
+      text = "jelo",
+      timestamp = "12:43:39 16-01-2026 "
+    } }
+}
